97f4cab1dfae64c7091fc9578e241cead8df5820
imdyangs <d65yang@gmail.com>
2020-12-03 02:11:21 -0500

    shogun challenge

--- README.md
+++ README.md
@@ -29,42 +29,18 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `yarn eject`
+---
 
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+# Frontend Jobs Challenge
+Create a React app that uses a public graphql API. The API is on top of a database that has information about Jobs (i.e. Software Engineer at Google, etc.) The playground and schema are available [here](https://api.graphql.jobs/).
 
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
+User scenario
+As a user, I would like to see a list of recent Job openings. I would also like to be able to see Jobs by city as I'm considering relocating. In addition to seeing an overall list view of open jobs, I'd like to be able to click through and view more details about a specific job.
 
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
+Implementation
+This challenge doesn't have many requirements on purpose. We'd like to see how you handle ambiguity. Keep the design clean and simple. Functionality is the biggest thing we'll look at. Code quality, style, and design are all secondary.
 
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
+Let me know if you have any questions!
 
-## Learn More
+- Zak
 
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
--- src/App.js
+++ src/App.js
@@ -1,25 +1,173 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown'
 import './App.css';
 
-function App() {
+const url = "https://api.graphql.jobs/"
+const query = `
+  query {
+    jobs {
+      id
+      title
+      cities {
+        name
+        id
+      }
+      description
+      company {
+        id
+        name
+        websiteUrl
+      }
+      postedAt
+    }
+  }
+`
+
+const cleanData = jobs => {
+  return jobs.map(job => {
+    if (job.cities.length === 0) {
+      job.cities[0] = 'NO_CITY';
+      job.citiesString = "Remote / No Location"
+    }
+    else if (job.cities.length === 1) {
+      job.cities = [job.cities[0].name];
+      job.citiesString = job.cities[0];
+    }
+    else {
+      job.cities = job.cities.map(city => city.name)
+      job.citiesString = job.cities.join(" • ")
+    }
+    job.postedAt = new Date(job.postedAt);
+    return job;
+  })
+}
+
+const sortJobsByRecentDate = jobs => {
+  return jobs.sort((a, b) => {
+    return b.postedAt - a.postedAt;
+  })
+}
+
+const sortJobsByCity = jobs => {
+  return jobs.sort((a, b) => {
+    return a.citiesString - b.citiesString;
+  })
+}
+
+const JobTab = ({ job, jobs }) => {
+  const [renderDescription, setRenderDescription] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="job-tab" style={{ backgroundColor: renderDescription ? 'rgb(131 130 140 / 10%)' : null }}>
+      <div className="job-vis" onClick={() => setRenderDescription(!renderDescription)}>
+        <div className="job-header">
+        <div className="job-title">
+          {job.title} • <b>{job.company.name}</b>
+        </div>
+        <div className="job-location">
+          {job.citiesString} • {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit"
+          }).format(job.postedAt)}
+        </div>
+      </div>
+       { renderDescription ?
+        <div className="job-description">
+          <div className="job-description-title">Description</div>
+          <ReactMarkdown>
+            {job.description}
+          </ReactMarkdown>
+          <div className="job-contact">
+            <div className="job-application-btn" onClick={() => window.open(job.company.websiteUrl)}>
+              Apply Here
+            </div>
+          </div>
+        </div> :
+        null
+       }
     </div>
-  );
+  </div>
+)}
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      jobs: [],
+      displayedJobs: [],
+      locationSearch: '',
+      jobsLoaded: false,
+    }
+    this.debouncedSetDisplayedJobs = debounce(jobs => {
+      this.setState({ displayedJobs: jobs })
+    }, 150);
+  }
+
+  componentDidMount() {
+    const opts = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query })
+    };
+
+    fetch(url, opts)
+      .then(res => res.json())
+      .then(({ data }) => {
+        let jobs = cleanData(data.jobs);
+        jobs = sortJobsByRecentDate(jobs);
+        this.setState({ jobs, displayedJobs: jobs, jobsLoaded: true });
+      })
+      .catch(console.error);
+  }
+
+  handleChange = e => {
+    e.preventDefault();
+
+    this.setState({ locationSearch: e.target.value }, () => {
+      if (this.state.locationSearch === '') {
+        this.debouncedSetDisplayedJobs(this.state.jobs)
+        // this.setState({ displayedJobs: this.state.jobs });
+      } else {
+        const filteredJobs = this.state.jobs.filter(job => job.citiesString.toLowerCase().includes(e.target.value));
+        this.debouncedSetDisplayedJobs(filteredJobs)
+        // this.setState({ displayedJobs: filteredJobs });
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div className="container">
+          <div className="title">
+            Walmart AngelList
+          </div>
+          <div className="job-filters">
+            Search jobs by city {" "}
+            <input className="job-location-search" value={this.state.locationSearch} onChange={this.handleChange} />
+          </div>
+          <div className="open-positions">
+            Open Positions
+          </div>
+          <div className="job-content">
+             { this.state.displayedJobs.length !== 0 || this.state.jobsLoaded ?
+              this.state.displayedJobs.map(job => <JobTab key={job.id} job={job} jobs={this.state.jobs} />) :
+              <div className="loader" /> }
+          </div>
+          </div>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// Helpers
+function debounce(func, wait) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func.apply(context, args), wait);
+  }
+}
